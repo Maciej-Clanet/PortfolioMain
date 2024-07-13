d04fe1f2d1b7816003f5f9e612b4ff8ce45914fd
Maciej-Clanet <43215153+Maciej-Clanet@users.noreply.github.com>
2024-07-14 00:49:36 +0100

    icons, skill content, file downloads

--- scripts/cv.js
+++ scripts/cv.js
@@ -1,19 +1,31 @@
 const experiences = {
     "cv-pos" : {
-        "heading" : "POSperity Software",
-        "description" : `test`
+        "heading" : `<h2 class="heading-text">POSperity Software</h2>`,
+        "description" : `
+        <p class="paragraph-text">I’m a part owner of the startup and a full stack developer on our Point of Sale system designed for restaurants.</p>
+        <p class="paragraph-text">While my main responsibility is creating the back-end with Rust, I routinely work on the Front-end using Typescript and React.</p>
+        <p class="paragraph-text">I’m also the primary designer, responsible for all UI/UX of the application.</p>
+        <p class="paragraph-text"><a href="./development/pos.html">Read more about the POS here →</a></p>
+        `
     },
     "cv-lecturer-uxb" : {
-        "heading" : "Lecturer at Uxbridge",
-        "description" : `test`
+        "heading" : `<h2 class="heading-text">Lecturer at Uxbridge</h2>`,
+        "description" : `
+        <p class="paragraph-text">I have been working at Uxbridge College, teaching web development and computer science to Level 2 and Level 3 students.</p> 
+        <p class="paragraph-text">The topics I teach include React, FastAPI, Figma, Fundamentals of Computing, and Cybersecurity and Encryption. I am responsible for all of the technical subjects for the Level 2 web development course and the Level 3 T-Level courses.</p>
+        `
     },
-    "cv-pod" : {
-        "heading" : "Print on Demand",
-        "description" : `test`
+    "cv-pod": {
+        "heading": `<h2 class="heading-text">Print on Demand</h2>`,
+        "description": `
+        <p class="paragraph-text">I create designs that get printed on t-shirts and similar items. I'm looking to launch a t-shirt brand soon.</p>
+        `
     },
-    "cv-lecturer-hayes" : {
-        "heading" : "Lecturer at Hayes",
-        "description" : `test`
+    "cv-lecturer-hayes": {
+        "heading": `<h2 class="heading-text">Lecturer at Hayes</h2>`,
+        "description": `
+        <p class="paragraph-text">I taught web development and C++ programming at Hayes College. I have also taught other subjects like computer hardware and e-commerce.</p>
+        `
     }
 }
 
@@ -22,13 +34,12 @@ const experiences = {
 
 function changeEntryDescription(choice){
 
-
     var descriptionBox = document.getElementById("cv-entry-description")
     choiceInfo = experiences[choice];
 
     descriptionBox.innerHTML = `
-        <h2 class="heading-text">${choiceInfo.heading}</h2>
-        <p class="paragraph-text">${choiceInfo.description}</p>
+        ${choiceInfo.heading}
+        ${choiceInfo.description}
     `
 
     var allEntries = document.getElementsByClassName("cv-entry")
@@ -37,7 +48,6 @@ function changeEntryDescription(choice){
     };
     document.getElementById(choice).classList.add("active");
 }
-
 
 //for keyboard navigation of job experience
 document.addEventListener('DOMContentLoaded', () => {
@@ -51,55 +61,4 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
-
-
-
-    // //email to clipboard functionality
-    // const copyButton = document.getElementById("copy-button");
-    // const emailText = document.getElementById("email-text").textContent;
-
-    // copyButton.addEventListener("click", () => {
-    //     navigator.clipboard.writeText(emailText).then(() => {
-    //         showNotification("Email address copied to clipboard");
-    //     }).catch(err => {
-    //         showNotification("Failed to copy email", true);
-    //         console.error("Failed to copy email: ", err)
-    //     });
-    // });
-
-
-    // //compose email
-    // const composeButton = document.getElementById("compose-button");
-    // const emailAddress = emailText;
-
-    // composeButton.addEventListener("click", () => {
-    //     window.location.href = `mailto:${emailAddress}`
-    // });
 });
-
-// function showNotification(message, isError = false){
-//     const notificationContainer = document.getElementById('notification-container');
-//     const notification = document.createElement('div');
-//     notification.classList.add('notification');
-
-//     if (isError) {
-//         notification.style.backgroundColor = '#e74c3c'; // Red background for errors
-//     }
-
-//     notification.textContent = message;
-//     notificationContainer.appendChild(notification);
-
-//     //show notification
-//     setTimeout(() => {
-//         notification.classList.add("show");
-//     }, 10);
-
-//     setTimeout(() => {
-//         notification.classList.remove("show");
-//         notification.classList.add("hide");
-
-//         notification.addEventListener("transitioned", () => {
-//             notification.remove();
-//         });
-//     }, 2000);
-// }
